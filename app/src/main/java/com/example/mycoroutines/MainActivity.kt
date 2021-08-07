a28b09aae7c1d7d452dc2398e8cdb0a021f24470
Ryuji Oda <ryuji.oda@gmail.com>
2021-08-07 15:17:28 +0900

    test: simple try catch

--- app/src/main/java/com/example/mycoroutines/MainActivity.kt
+++ app/src/main/java/com/example/mycoroutines/MainActivity.kt
@@ -28,28 +28,65 @@ class MainActivity : AppCompatActivity() {
     //    runBlockingは新しいCoroutineScopeを作成し、その中で起動された全てのCoroutineが完了するまで処理をブロックします
     fun main() {
         runBlocking {
-            fun job1() = launch {
-                println("11")
-                delay(1000L)
-                println("22")
-                delay(1000L)
-                println("job1 end")
+
+            val job1 = launch {
+                try {
+                    println("11")
+                    delay(1000L)
+                    println("22")
+                    delay(1000L)
+                    println("job1 end")
+                } catch (e: CancellationException) {
+                    // delay等のキャンセル可能な中断関数はCancellationExceptionを吐きます
+                    println("job1 $e")
+                }
+
             }
-            fun job2() = launch {
+            val job2 = launch {
                 println("111")
                 delay(1000L)
                 println("222")
                 delay(1000L)
                 println("job2 end")
             }
+
             kotlin.run {
-                job1()
-                job2()
-            }.join()
+                job1
+                job2
+            }
             // join をつけない場合またずにall endは実行される
-            println("all end")
+            //println("all end")
+            delay(500L)
+            job1.cancel() // 特定のJobだけキャンセルできる
 
             // Thread.sleep(2000L) // 処理が完了するまで待機
+
+            launch {
+                try {
+                    println("try1")
+                    delay(1000L)
+                    println("try2")
+                    delay(1000L)
+                    throw Exception("error!")
+                } catch (e: Throwable) {
+                    println("exception: $e")
+                }
+            }
         }
     }
+
+//    fun job1(scope: CoroutineScope) = scope.launch {
+//        println("11")
+//        delay(1000L)
+//        println("22")
+//        delay(1000L)
+//        println("job1 end")
+//    }
+//    fun job2(scope: CoroutineScope) = scope.launch {
+//        println("111")
+//        delay(1000L)
+//        println("222")
+//        delay(1000L)
+//        println("job2 end")
+//    }
 }
